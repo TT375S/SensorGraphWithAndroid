7361075533846683ee4fd71389a66545fc070097
TT375S <tomataka375@gmail.com>
2017-06-29 19:20:34 +0900

    Change values. It shows "velocity" but acceleration.

--- app/src/main/java/com/example/user1/sensorgraphwithandroid/MainActivity.java
+++ app/src/main/java/com/example/user1/sensorgraphwithandroid/MainActivity.java
@@ -23,6 +23,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     TextView yTextView;
     TextView zTextView;
 
+    LineChart mChart;
+
+    String[] names = new String[]{"x-value", "y-value", "z-value"};
+    int[] colors = new int[]{Color.RED, Color.GREEN, Color.BLUE};
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,7 +38,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         zTextView = (TextView)findViewById(R.id.zValue);
 
         manager = (SensorManager)getSystemService(SENSOR_SERVICE);
-        sensor = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensor = manager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
 
         mChart = (LineChart) findViewById(R.id.lineChart);
 
@@ -41,11 +47,58 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
 
 
+    private float sensorX;
+    private float sensorY;
+    private float sensorZ;
+    final private float k = 0.1f;   //値が大きいほどローパスフィルタの効きが強くなる
+    private float lowPassX = 0,lowPassY = 0,lowPassZ = 0; //ローパスフィルタ通過後の値
+
+    private float rawAx = 0, rawAy=0, rawAz = 0;    //ハイパスフィルタを通した値
+
+    long oldTime = 0;   //前回、センサの値が変更されたとき
+
+    float vx = 0, vy = 0, vz = 0;   //速度だけど端末から見た座標(ローカル座標)なことに注意
+    float x=0, y=0, z=0;    //位置。精度悪くてあてにならない。
+
     @Override
     public void onSensorChanged(SensorEvent event) {
-        xTextView.setText(String.valueOf(event.values[0]));
-        yTextView.setText(String.valueOf(event.values[1]));
-        zTextView.setText(String.valueOf(event.values[2]));
+
+        //センサーの生の値
+        sensorX = event.values[0];
+        sensorY = event.values[1];
+        sensorZ = event.values[2];
+
+        //LPF
+        lowPassX += (event.values[0] - lowPassX) * k;
+        lowPassY += (event.values[1] - lowPassY) * k;
+        lowPassZ += (event.values[2] - lowPassZ) * k;
+
+        // High Pass Filter
+        rawAx = event.values[0] - lowPassX;
+        rawAy = event.values[1] - lowPassY;
+        rawAz = event.values[2] - lowPassZ;
+
+
+        if(oldTime == 0) oldTime = System.currentTimeMillis();
+        long nowTime = System.currentTimeMillis();
+        long interval = nowTime - oldTime;
+        oldTime = nowTime;
+
+        vx += rawAx * interval / 10; // [cm/s] にする
+        vy += rawAy * interval / 10;
+        vz += rawAz * interval / 10;
+        x += vx * interval / 1000; // [cm] にする
+        y += vy * interval / 1000;
+        z += vz * interval / 1000;
+
+        float temp[] = new float[3];
+        temp[0] = vx;
+        temp[1] = vy;
+        temp[2] = vz;
+
+        xTextView.setText(String.valueOf(vx));
+        yTextView.setText(String.valueOf(vy));
+        zTextView.setText(String.valueOf(vz));
 
 
         LineData data = mChart.getLineData();
@@ -93,10 +146,4 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         super.onPause();
         manager.unregisterListener(this);
     }
-
-    LineChart mChart;
-
-    String[] names = new String[]{"x-value", "y-value", "z-value"};
-    int[] colors = new int[]{Color.RED, Color.GREEN, Color.BLUE};
-
 }
